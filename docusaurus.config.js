e6fa3611f58c6a621d800b818e7f260ca6e6c296
Altaf Ali <7231912+altaf-ali@users.noreply.github.com>
2023-05-16 21:49:17 +0100

    🔧 chore(deploy.yml): add environment variables for URL and BASE_URL 🔧 chore(docusaurus.config.js): use environment variables for URL and BASE_URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://softpack.pages.dev',
+  url: process.env.URL,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.BASE_URL,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
